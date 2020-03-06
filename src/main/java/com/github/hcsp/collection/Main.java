cf2a6d0b95e61057c6e4c1cc81fac9d6ccbfb450
pathjh <1639925487@qq.com>
2020-03-06 19:37:26 +0800

    hsahset与hashmap (#105)
    hsahset与hashmap (#105)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,15 +1,29 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> set = new HashSet<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        for (Person pa : a
+        ) {
+            map.put(pa.getName(), 1);
+        }
+        for (Person pb : b
+        ) {
+            if (map.containsKey(pb.getName())) {
+                set.add(pb);
+            } else {
+                map.put(pb.getName(), 1);
+            }
+        }
+        return set;
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
-    public static class Person {
+    public static class Person implements Comparable<Person> {
         private String name;
 
         public Person(String name) {
@@ -22,6 +36,34 @@ public class Main {
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        @Override
+        public int compareTo(Person person) {
+            return this.name.compareTo(person.name);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            } else if (!(o instanceof Person)) {
+                return false;
+            }
+            Person person = (Person) o;
+            return name.equals(person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
         }
     }
 
