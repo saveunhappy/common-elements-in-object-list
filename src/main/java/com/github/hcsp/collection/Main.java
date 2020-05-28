44aef61a8d2cc9769bebdb19b1feae7c136509fe
lionlihuang <65241440+lionlihuang@users.noreply.github.com>
2020-05-28 12:42:59 +0800

    求对象列表的公共元素 (#138)
    求对象列表的公共元素 (#138)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,12 +1,20 @@
 package com.github.hcsp.collection;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
+
+
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> seta = new HashSet<>(a);
+        Set<Person> setb = new HashSet<>(b);
+        seta.retainAll(setb);
+        return seta;
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -14,6 +22,19 @@ public class Main {
 
         public Person(String name) {
             this.name = name;
+        }
+        @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Person) {
+                return this.getName() == ((Person) obj).getName();
+
+            }
+            return false;
         }
 
         public String getName() {
