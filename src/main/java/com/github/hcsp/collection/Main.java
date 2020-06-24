b21bd7301dabf045e8437441d1896859816f420e
xuepan-summer <54898607+xuepan-summer@users.noreply.github.com>
2020-06-24 15:36:01 +0800

    重写equals和hashCode() (#145)
    重写equals和hashCode() (#145)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -3,10 +3,22 @@ package com.github.hcsp.collection;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.HashSet;
+import java.util.Objects;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> commonElement = new HashSet<>();
+        for (Person person1 : a) {
+            for (Person person2 : b) {
+                if (person1.equals(person2)) {
+                    commonElement.add(person1);
+                }
+            }
+        }
+        return commonElement;
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -22,6 +34,23 @@ public class Main {
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
         }
     }
 
