4981fada4a3934aa9e7d0be759e66f4fe2d43028
qiaomengnan16 <qiaomengnan2020@outlook.com>
2020-09-13 08:27:09 +0800

    get common elements (#161)
    get common elements (#161)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -2,11 +2,15 @@ package com.github.hcsp.collection;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        return a.stream().filter(temp -> b.contains(temp)).collect(Collectors.toSet());
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -23,6 +27,17 @@ public class Main {
         public void setName(String name) {
             this.name = name;
         }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(this.name);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj != null && obj.getClass() == this.getClass() && ((Person) obj).getName().equals(this.getName());
+        }
+
     }
 
     public static void main(String[] args) {
