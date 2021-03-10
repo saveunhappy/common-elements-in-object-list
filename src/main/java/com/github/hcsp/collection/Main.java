fc1230ce0ae3a29a5c36e26c90fd638f78396ee8
chengpenggm <79912150+chengpenggm@users.noreply.github.com>
2021-03-10 16:52:14 +0800

    Set求交集，import 去除星号 (#182)
    Set求交集，import 去除星号 (#182)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,12 +1,20 @@
 package com.github.hcsp.collection;
 
+
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        HashSet<Person> aSet = new HashSet<>(a);
+        HashSet<Person> bSet = new HashSet<>(b);
+        aSet.retainAll(bSet);
+        return new HashSet<>(aSet);
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -22,6 +30,23 @@ public class Main {
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Person person = (Person) o;
+            return name.equals(person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
         }
     }
 
