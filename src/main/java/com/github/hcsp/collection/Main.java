4b28271f321d830ced1f0b1e4ace8784350fe395
LiuXing <94578246+iamxliu@users.noreply.github.com>
2022-04-20 14:47:23 +0800

    hashcode (#214)
    hashcode (#214)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,16 +1,34 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> seta = new HashSet<>(a);
+        Set<Person> setb = new HashSet<>(b);
+        seta.retainAll(setb);
+        return seta;
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
         private String name;
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj ) {
+                return true;
+            } else if (obj == null || this.getClass() != obj.getClass()) {
+                return false;
+            }
+            return this.name == ((Person) obj).name;
+        }
 
         public Person(String name) {
             this.name = name;
