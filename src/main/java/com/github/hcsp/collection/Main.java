c677773f44d0b42aa17ee0de254375f080f9a1c3
yangyangsimons <97254476+yangyangsimons@users.noreply.github.com>
2022-06-25 19:04:42 +0100

    extract the common elements in the list of a and b (#219)
    extract the common elements in the list of a and b (#219)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,12 +1,18 @@
 package com.github.hcsp.collection;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> commonNameSet = new HashSet<>(a);
+        commonNameSet.retainAll(b);
+        return commonNameSet;
+
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
@@ -22,6 +28,21 @@ public class Main {
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Person p = (Person) obj;
+            if (this.name.hashCode() == p.name.hashCode()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public int hashCode() {
+            return name.hashCode();
         }
     }
 
